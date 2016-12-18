c03ab879bfa520c4a84a2d766263f21be85cc731
sergeykamyshov <kamishovsergey@gmail.com>
2016-12-18 21:42:32 +0300

    Added ArrayAdapter with test data

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,12 +1,26 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+
+        List<String> words = new ArrayList<>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+        ListView numbersList = (ListView) findViewById(R.id.numbersList);
+        numbersList.setAdapter(itemsAdapter);
     }
 }
