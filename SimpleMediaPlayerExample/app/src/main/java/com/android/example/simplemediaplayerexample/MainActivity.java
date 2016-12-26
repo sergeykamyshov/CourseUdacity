c22f725eda7752b311688b6950793bdb8e5ec468
sergeykamyshov <kamishovsergey@gmail.com>
2016-12-26 21:41:30 +0300

    Added 3 buttons to play, pause and stop song

--- SimpleMediaPlayerExample/app/src/main/java/com/android/example/simplemediaplayerexample/MainActivity.java
+++ SimpleMediaPlayerExample/app/src/main/java/com/android/example/simplemediaplayerexample/MainActivity.java
@@ -1,13 +1,36 @@
 package com.android.example.simplemediaplayerexample;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view) {
+        if (mMediaPlayer == null) {
+            mMediaPlayer = MediaPlayer.create(this, R.raw.cheap_thrills);
+        }
+        mMediaPlayer.start();
+    }
+
+    public void pause(View view) {
+        mMediaPlayer.pause();
+    }
+
+    public void stop(View view) {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.stop();
+            mMediaPlayer.reset();
+            mMediaPlayer = null;
+        }
     }
 }
