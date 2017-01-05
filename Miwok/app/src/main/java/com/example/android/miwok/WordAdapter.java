c8320f44e88016a2566d262446cb15eae477eff6
sergeykamyshov <kamishovsergey@gmail.com>
2017-01-05 23:36:03 +0300

    Added play icon on liste item

--- Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,7 +1,6 @@
 package com.example.android.miwok;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
@@ -9,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -46,8 +46,10 @@ public class WordAdapter<T> extends ArrayAdapter<T> {
         }
 
         View textContainer = listItemView.findViewById(R.id.text_container);
+        LinearLayout playLinearLayout = (LinearLayout) listItemView.findViewById(R.id.play_linear_layout);
         int color = ContextCompat.getColor(getContext(), mColorResourceId);
         textContainer.setBackgroundColor(color);
+        playLinearLayout.setBackgroundColor(color);
 
         return listItemView;
     }
