fbb1ad137beae089c3f3275c87e1c7f1482e919e
sergeykamyshov <kamishovsergey@gmail.com>
2017-01-14 22:50:48 +0300

    Added ViewPager. Now you can change fragment by swipping right and left

--- Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,51 +15,21 @@
  */
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        TextView numbersTextView = (TextView) findViewById(R.id.numbers);
-        numbersTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), NumbersActivity.class));
-            }
-        });
-
-        TextView phrasesTextView = (TextView) findViewById(R.id.phrases);
-        phrasesTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), PhrasesActivity.class));
-            }
-        });
-
-        TextView familyTextView = (TextView) findViewById(R.id.family);
-        familyTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), FamilyActivity.class));
-            }
-        });
-
-        TextView colorsTextView = (TextView) findViewById(R.id.colors);
-        colorsTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), ColorsActivity.class));
-            }
-        });
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        PagerAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
     }
 }
